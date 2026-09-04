7ca4b6bba7ea82d8bcd47a2640ffa20454ce0018
ns9590679-cpu <ns9590679@gmail.com>
2026-09-04 23:41:36 +0530

    Create Main.jsx

---
+++ Main.jsx
@@ -0,0 +1,13 @@
+src/
+ ├─ components/
+ │   ├─ Hero.jsx
+ │   ├─ About.jsx
+ │   ├─ Projects.jsx
+ │   ├─ Skills.jsx
+ │   ├─ ImageStrip.jsx
+ │   ├─ Testimonials.jsx
+ │   ├─ Contact.jsx
+ │   └─ Footer.jsx
+ ├─ App.jsx
+ ├─ index.css
+ └─ main.jsx
